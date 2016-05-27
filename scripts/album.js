063bba6bf3583750d5ffe4ddc9551412060cad07
Ted <ekeast328@gmail.com>
2016-05-27 15:15:37 -0400

    Assignment 25 Complete

--- scripts/album.js
+++ scripts/album.js
@@ -1,52 +1,68 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21' },
-        { title: 'Magenta', duration: '2:15' }
-    ]
-};
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
 
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumMetallica = {
+    title: 'Ride the Lightning',
+    artist: 'Metallica',
+    label: 'Elektra',
+    year: '1984',
     albumArtUrl: 'assets/images/album_covers/20.png',
     songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21' },
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15' }
+        { title: 'Battery', duration: '6:00'},
+        { title: 'Master of Puppets', duration: '8:00'},
+        { title: 'The Thing That Should Not Be', duration: '5:00'},
+        { title: 'Sanitarium', duration: '6:00'}
     ]
 };
 
-var createSongRow = function(songNumber, songName, songLength) {
-    var template = 
+ var createSongRow = function(songNumber, songName, songLength) {
+     var template =
         '<tr class="album-view-song-item">'
-        '   <td class="song-item-number">' + songNumber + '</td>'
-        '   <td class="song-item-title">' + songName + '</td>'
-        '   <td class="song-item-duration">' + songLength + '</td>'
-        '</td>'
-        ;
-    return template;
-};
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+     return template;
+ };
 
- var setCurrentAlbum = function(album) {
-     // #1
+var setCurrentAlbum = function(album) {
+
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
+
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -62,3 +78,15 @@ var createSongRow = function(songNumber, songName, songLength) {
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
  };
+
+var albumArray = [albumPicasso, albumMarconi, albumMetallica];
+
+var albumNumber = 0;
+
+document.getElementsByClassName('album-cover-art')[0].addEventListener('click', function(){ 
+    albumNumber += 1;
+    if (albumNumber == albumArray.length ) {
+        albumNumber = 0;
+    }
+    setCurrentAlbum(albumArray[albumNumber]); });
+    
